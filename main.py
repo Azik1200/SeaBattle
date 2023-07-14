a38c665faf0d93d20e6d04a45f52be77708b9813
Aziz Salimli <azizsalimoglu02@gmail.com>
2023-07-14 13:58:43 +0400

    Button added

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ size_canvas_y = 600
 s_x = s_y = 10  # Field size
 step_x = size_canvas_x // s_x
 step_y = size_canvas_y // s_y
+size_canvas_x = step_x*s_x
+size_canvas_y = step_y*s_y
 
+menu_x = 250
 
 def on_closing():
     global app_running
@@ -23,7 +26,7 @@ tk.protocol("WM_DELETE_WINDOW", on_closing)
 tk.title("See Battle")
 tk.resizable(False, False)
 tk.wm_attributes("-topmost", 1)  # TODO Delete after ending project
-canvas = Canvas(tk, width=size_canvas_x, height=size_canvas_y, bd=0, highlightthickness=0)
+canvas = Canvas(tk, width=size_canvas_x+menu_x, height=size_canvas_y, bd=0, highlightthickness=0)
 canvas.create_rectangle(0, 0, size_canvas_x, size_canvas_y, fill="white")
 canvas.pack()
 tk.update()
@@ -37,6 +40,17 @@ def draw_table():
 
 
 draw_table()
+
+def button_show_enemy():
+    pass
+
+def button_restart():
+    pass
+
+b0 = Button(tk, text="Show enemy ship", command=button_show_enemy)
+b0.place(x=size_canvas_x+20, y=30)
+b1 = Button(tk, text="Restart", command=button_restart)
+b1.place(x=size_canvas_x+20, y=70)
 
 while app_running:
     if app_running:
