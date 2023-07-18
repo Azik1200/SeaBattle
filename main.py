871289cfd3644792d091f5eaa059450f6282df23
Aziz Salimli <azizsalimoglu02@gmail.com>
2023-07-18 16:01:06 +0400

    Draw point is ready

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ ship_len3 = s_x // 2
 enemy_ships = [[0 for i in range(s_x + 1)] for i in range(s_y + 1)]
 list_ids = []
 
+
 def on_closing():
     global app_running
     if messagebox.askokcancel("Exit", "Do you want to exit?"):
@@ -55,8 +56,9 @@ def button_show_enemy():
         for j in range(0, s_y):
             if enemy_ships[j][i] > 0:
                 _id = canvas.create_rectangle(i * step_x, j * step_y, i * step_x + step_x, j * step_y + step_y,
-        fill="red")
+                                              fill="red")
                 list_ids.append(_id)
+
 
 def button_restart():
     global list_ids
@@ -72,6 +74,23 @@ b1 = Button(tk, text="Restart", command=button_restart)
 b1.place(x=size_canvas_x + 20, y=70)
 
 
+def draw_point(x, y):
+    if enemy_ships[y][x] == 0:
+        color = "red"
+        id1 = canvas.create_oval(x * step_x, y * step_y, x * step_x + step_x, y * step_y + step_y, fill=color)
+        id2 = canvas.create_oval(x * step_x+step_x//3, y * step_y+step_y//3, x * step_x + step_x - step_x//3, y * step_y + step_y - step_y//3, fill="white")
+        list_ids.append(id1)
+        list_ids.append(id2)
+    else :
+        color="blue"
+        id1 = canvas.create_rectangle(x * step_x, y * step_y + step_y // 2 - step_y // 10, x * step_x + step_x,
+                                      y * step_y + step_y // 2 + step_y // 10, fill=color)
+        id2 = canvas.create_rectangle(x * step_x + step_x // 2 - step_x // 10, y * step_y,
+                                      x * step_x + step_x // 2 + step_x // 10, y * step_y + step_y, fill=color)
+        list_ids.append(id1)
+        list_ids.append(id2)
+
+
 def add_to_all(event):
     _type = 0
     if event.num == 3:
@@ -79,8 +98,10 @@ def add_to_all(event):
     mouse_x = canvas.winfo_pointerx() - canvas.winfo_rootx()
     mouse_y = canvas.winfo_pointery() - canvas.winfo_rooty()
     ip_X = mouse_x // step_x
-    ip_y = mouse_y // step_y
+    ip_Y = mouse_y // step_y
     # print(ip_X, ip_y, "_type", _type)
+    if ip_X < s_x and ip_Y < s_y:
+        draw_point(ip_X, ip_Y)
 
 
 canvas.bind_all("<Button-1>", add_to_all)
@@ -93,7 +114,7 @@ def generate_enemy_ships():
     # генерируем список случайных длин кораблей
     for i in range(0, ships):
         ships_list.append(random.choice([ship_len1, ship_len2, ship_len3]))
-    #print(ships_list)
+    # print(ships_list)
 
     # подсчет суммарной длины кораблей
     sum_1_all_ships = sum(ships_list)
@@ -159,10 +180,9 @@ def generate_enemy_ships():
                 if enemy_ships[j][i] > 0:
                     sum_1_enemy = sum_1_enemy + 1
 
-        #print(sum_1_enemy)
-        #print(ships_list)
-        #print(enemy_ships)
-
+        # print(sum_1_enemy)
+        # print(ships_list)
+        # print(enemy_ships)
 
 
 generate_enemy_ships()
